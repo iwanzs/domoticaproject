dd70a5124714dc29eb09c4113888d3ed2615746c
Skahir <33893915+Skahir@users.noreply.github.com>
2020-06-16 18:57:10 +0200

    still working is in progress

--- Applicatie/GarduinoApp/Week4/Views/HistoryPage.xaml.cs
+++ Applicatie/GarduinoApp/Week4/Views/HistoryPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OxyPlot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ namespace GarduinoApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HistoryPage : ContentPage
     {
+
+        PlotModel Graph;
+
         public HistoryPage()
         {
             InitializeComponent();
