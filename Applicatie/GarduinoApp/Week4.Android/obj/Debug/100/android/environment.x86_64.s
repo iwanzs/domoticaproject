c4bed515e12056a33c61da677f3d76b0070a17c0
Skahir <33893915+Skahir@users.noreply.github.com>
2020-06-19 12:33:42 +0200

    Revert "Revert "Working graph that updates every 5 min""
    This reverts commit 82d8b84847b3addde13cf66f9c4341695545e239.

--- Applicatie/GarduinoApp/Week4.Android/obj/Debug/100/android/environment.x86_64.s
+++ Applicatie/GarduinoApp/Week4.Android/obj/Debug/100/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"89f7d322-95c4-4d8a-acfd-aa58bc944819"
+	.asciz	"6b210277-68a6-49ce-98cb-bcea78a07bc8"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- Applicatie/GarduinoApp/Week4/Views/HistoryPage.xaml.cs
+++ Applicatie/GarduinoApp/Week4/Views/HistoryPage.xaml.cs
@@ -16,6 +16,18 @@ namespace GarduinoApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HistoryPage : ContentPage, INotifyPropertyChanged
     {
+        //max amount of points in a graph
+        private int MAXPOINTS = 10;
+
+        /// <summary>
+        /// 0 = airtemp
+        /// 
+        /// default is 0
+        /// </summary>
+        public int TYPEOFSENSOR = 0;
+
+        public TimeSpan STARTTIMESPAN = TimeSpan.Zero;
+        public TimeSpan PERIODTIMESPAN = TimeSpan.FromMinutes(5);
 
         public PlotModel Graph { get; set; }
         public LineSeries lineSeries { get; set; }
@@ -25,29 +37,69 @@ namespace GarduinoApp.Views
             InitializeComponent();
 
             CreateGraph();
-            AddPoint(30, 20);
-            AddPoint(40, 25);
+            //mock data
+
+            //for testing
+            var rnd = new Random();
+
+            AddPoint(DateTime.Now, rnd.Next(0, 40));
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                AddPoint(DateTime.Now, rnd.Next(0,40));
+            }, null, STARTTIMESPAN, PERIODTIMESPAN);
+
+            //var timer = new System.Threading.Timer((e) =>
+            //{
+            //    AddPoint(new DateTime(), passed value);
+            //}, null, STARTTIMESPAN, PERIODTIMESPAN);
         }
 
         public void CreateGraph()
         {
-            Graph = new PlotModel { Title = "a graph"};
-            Graph.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "Time" });
-            Graph.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Temperature(℃)" });
+            switch (TYPEOFSENSOR)
+            {
+                case 0:
+                    Graph = new PlotModel { Title = "Temp of air" };
+                    //Graph.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "Time" });
+                    Graph.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, StringFormat = "HH:mm dd-MM-yyyy", Title = "Time" });
+                    Graph.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Temperature(℃)" });
 
-            lineSeries = new LineSeries();
-            lineSeries.Points.Add(new DataPoint(0, 20));
-            lineSeries.Points.Add(new DataPoint(10, 22));
-            lineSeries.Points.Add(new DataPoint(20, 15));
-            Graph.Series.Add(lineSeries);
-            LineGraph.Model = Graph;
+                    lineSeries = new LineSeries();
+                    Graph.Series.Add(lineSeries);
+                    LineGraph.Model = Graph;
+                    break;
+
+                default:
+                    break;
+            }
         }
 
-        public void AddPoint(int time, float temp)
+        public void AddPoint(DateTime time, float temp)
         {
-            Graph.Series.Remove(lineSeries);
-            lineSeries.Points.Add(new DataPoint(time, temp));
-            Graph.Series.Add(lineSeries);
+            switch (TYPEOFSENSOR)
+            {
+                case 0:
+                    Graph.Series.Remove(lineSeries);
+                    lineSeries.Points.Add(new DataPoint(DateTimeAxis.ToDouble(time), temp));
+                    Graph.Series.Add(lineSeries);
+                    break;
+
+
+                default:
+                    break;
+            }
+
+            //removes a point if the max is hit
+            if (lineSeries.Points.Count >= MAXPOINTS)
+            {
+                RemovePoint();
+            }
+        }
+
+        public void RemovePoint()
+        {
+            lineSeries.Points.RemoveAt(0);
         }
 
     }
